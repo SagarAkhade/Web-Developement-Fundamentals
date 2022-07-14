3d17a687bbd9e7e04255cc95efa79c512c9ecd4f
Sagar Akhade <104863414+SagarAkhade@users.noreply.github.com>
2022-07-14 16:03:10 +0530

    Create  2. Variables in JavaScript.js

---
+++ JavaScript(Basic to Advance)/2. Variables in JavaScript.js
@@ -0,0 +1,40 @@
+// Variables in JavaScript
+// Variable ->> use to store some information 
+// declaration of variables 
+//TypeOfVariable VariableName = value;
+// Rules for declaring variables ->> 
+// 1. Cannot start with number, Ex - 1value (invalid) , value1 (valid)
+// 2. Can only use underscore _ or Dollar $ in variables names(No other symbol allowed), Ex: _name  (valid), na_me$ (valid)
+// 3. Cannot use spaces in variables Ex: var first name (invalid) var first_name (valid called as snake case writing)
+// 4. Convention ->> start with small letter and use camelCase Ex: firstName
+
+// Types of Data types in JavaScript ->> var , let , const 
+
+// var keyword
+var firstName = "Sagar";
+console.log(firstName);
+// Output : Sagar
+
+//change value
+firstName = "Mohit";
+console.log(firstName);
+// Output : Mohit
+
+"use strict"; // ->> use for developer to avoid mistakes
+
+// let keyword ->> 
+// Mostly let keyword(launched in ES5) is used for declaring varibles
+// Difference between var and let
+// var can be initialize multiple times with same variable name 
+//But in case of let, we cannot initialize with same variable name muliple times. It will gives error
+// Main difference is block scope vs function scope
+let name1 = "Sagar";
+console.log(name1);
+// Output : Sagar
+
+// Declaring Constant
+// Constants ->> these values never change Ex : pi
+// Constant value never changes it remains same in all program
+const pi = 3.14;
+console.log(pi);
+// Output : 3.14
