cf110de8e842ce7f5407852efbd9a30ed86c60dd
Sagar Akhade <104863414+SagarAkhade@users.noreply.github.com>
2022-07-17 19:03:03 +0530

    Create 11. Lexical Scope in JavaScript.js

---
+++ JavaScript(Basic to Advance)/11. Lexical Scope in JavaScript.js
@@ -0,0 +1,24 @@
+// lexical Scope in JavaScript 
+
+// When we calling any value of from function inside function
+// but that value is not present in that inside function
+// then it try to access ouside it but inside of main function
+// And if value is also not present in main function then
+// it goes to all file and check for value
+// this is called as Lexical Scope.
+// And this completely become Lexical environment(Or Lexical Chaining) for it.
+const myVar = "value1";
+function myApp() {  
+    function myFunc1(){ 
+        const myFunc2 = () => { 
+            console.log("inside myFunc", myVar);
+        }
+        myFunc2();
+    } 
+    console.log(myVar);
+    myFunc1();
+}
+myApp();
+// Output : 
+// value1
+// inside myFunc value1
