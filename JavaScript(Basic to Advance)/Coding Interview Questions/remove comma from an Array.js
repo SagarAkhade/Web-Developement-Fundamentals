a80d868d10550e575a2930afd77365f6a97c3d45
Sagar Akhade <104863414+SagarAkhade@users.noreply.github.com>
2022-08-31 00:19:01 +0530

    Create remove comma from an Array.js

---
+++ JavaScript(Basic to Advance)/Coding Interview Questions/remove comma from an Array.js
@@ -0,0 +1,8 @@
+// Q. remove comma from an Array
+var testarray = new Array("a", "b", "c");
+var str = "";
+for (var i = 0; i < testarray.length; i++) {
+    str += testarray[i] + " ";
+}
+console.log(str);
+// Output : a b c
